393e68a7bdf2dc8848b9912ec3e38352b439b623
mounikapratapa <43151963+mounikapratapa@users.noreply.github.com>
2019-05-02 15:50:26 -0400

    Update Okamoto_Uchiyama.py

--- Okamoto_Uchiyama.py
+++ Okamoto_Uchiyama.py
@@ -27,22 +27,17 @@ def modinv(a, m):
         return x % m
 def L(x, p):
     return ((x-1) // p)
-def keys(size = 12):
+def keys(size):
     p = generate_prime_numbers(size)
     q = generate_prime_numbers(size)
     n =  p*p*q
-    g =  random.getrandbits(size)
-    print(g)
-    if ((gcd(g,n) ==1) & (pow(g, p-1, p*p) != 1)):
-        g = g
-        print(g)
-    else:
-        print("cant find the right g")
+    g =  random.randint(23,n)
+    assert ((gcd(g,n) == 1) and (pow(g, p-1, p**2)!=1))
     h = pow(g, n, n)
     return((n, g, h), (p, q))
 def enc(pk,pt):
     n,g,h = pk
-    r = random.getrandbits(12)
+    r = random.randint(23,n)
     k1 = pow(g,pt,n)
     k2 = pow(h,r,n)
     ct =  (k1 * k2) % n
@@ -55,8 +50,9 @@ def dec(sk,g,ct):
     message = (L(x1,p) // L(x2,p)) % p
     return message
 def main():
-    pk, sk = keys()
+    pk, sk = keys(1024)
     n,g,h = pk
+    print(pk,sk)
     disp = input("Do you want to test for homomorphism?(y/n):")
     if(disp == 'y'):
         print("Good choice,let us have some fun!")
